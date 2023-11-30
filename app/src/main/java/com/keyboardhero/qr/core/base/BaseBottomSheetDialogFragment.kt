577875c2bff198985b6af98414f822d421b6be6d
NamTQ <tran.quang.nam@thundersoft.com>
2023-11-30 10:30:21 +0700

    add bottom sheet fragment

--- app/src/main/java/com/keyboardhero/qr/core/base/BaseBottomSheetDialogFragment.kt
+++ app/src/main/java/com/keyboardhero/qr/core/base/BaseBottomSheetDialogFragment.kt
@@ -1,87 +1,122 @@
 package com.keyboardhero.qr.core.base
 
+import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedDispatcherOwner
-import androidx.activity.addCallback
-import androidx.annotation.CallSuper
-import androidx.core.content.ContextCompat
+import android.view.WindowManager
 import androidx.core.view.isVisible
-import androidx.navigation.fragment.findNavController
-import androidx.viewbinding.ViewBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.keyboardhero.qr.databinding.FragmentBaseBinding
+import com.keyboardhero.qr.R
+import com.keyboardhero.qr.databinding.BaseButtomSheetLayoutBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * This abstract class should be used for all bottom sheet dialog fragment.
- *
- * @param VB : the [ViewBinding].
- * @property baseActivity the [BaseActivity].
- * @property baseBinding the [FragmentBaseBinding].
- * @property binding the [VB].
- * @property bindingInflater the logic binding inflate.
- */
-abstract class BaseBottomSheetDialogFragment<VB : ViewBinding> :
-    BottomSheetDialogFragment(),
-    IBaseFragment {
-    override val baseActivity: BaseActivity<*>?
-        get() = activity as? BaseActivity<*>
+@AndroidEntryPoint
+class BaseBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
-    private lateinit var baseBinding: FragmentBaseBinding
+    private var childFragment: Fragment? = null
 
-    lateinit var binding: VB
-        private set
-    abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+    private var animationRes: Int? = null
 
-    @CallSuper
+    private lateinit var binding: BaseButtomSheetLayoutBinding
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        animationRes?.let {
+            dialog?.window?.attributes?.windowAnimations = it
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
-        baseBinding = FragmentBaseBinding.inflate(inflater, container, false)
-        binding = bindingInflater.invoke(inflater, baseBinding.contentContainer, true)
-        initViews()
-        initActions()
-
-        return baseBinding.root
-    }
-
-    @CallSuper
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Make parent background transparent for showing rounded corner border of header
-        (view.parent as View).setBackgroundColor(
-            ContextCompat.getColor(
-                requireContext(),
-                android.R.color.transparent,
-            ),
+        binding = BaseButtomSheetLayoutBinding.inflate(inflater, container, false)
+        dialog?.window?.setSoftInputMode(
+            WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE
         )
+        return binding.root
+    }
 
-        initData(arguments)
-        initObservers()
-
-        (requireActivity() as OnBackPressedDispatcherOwner)
-            .onBackPressedDispatcher
-            .addCallback(viewLifecycleOwner) {
-                onBackPressed()
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            if (this is BottomSheetDialog) {
+                behavior.skipCollapsed = true
+                behavior.state = STATE_EXPANDED
             }
-    }
-
-    override fun showLoading() {
-        baseBinding.loadingView.root.isVisible = true
-    }
-
-    override fun hideLoading() {
-        baseBinding.loadingView.root.isVisible = false
-    }
-
-    override fun onBackPressed() {
-        if (!findNavController().popBackStack()) {
-            requireActivity().finish()
         }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        childFragment?.also { fragment ->
+            childFragmentManager.beginTransaction().apply {
+                replace(R.id.bottomSheetHolder, fragment)
+                commit()
+            }
+        }
+
+        (view.parent as? View)?.setBackgroundColor(Color.TRANSPARENT)
+
+        arguments?.getBoolean(KEY_HIDE_BOTTOM_TOGGLE_VIEW)?.let { isShow ->
+            binding.bottomSheetToggleView.isVisible = isShow
+        }
+    }
+
+    fun show(fragmentManager: FragmentManager) {
+        super.show(fragmentManager, null)
+    }
+
+    fun setDraggableState(isDraggable: Boolean) {
+        (dialog as? BottomSheetDialog)?.let {
+            it.behavior.isDraggable = isDraggable
+        }
+    }
+
+    companion object {
+        private const val KEY_HIDE_BOTTOM_TOGGLE_VIEW = "KEY_HIDE_BOTTOM_TOGGLE_VIEW"
+
+        fun newInstance(childFragment: Fragment): BaseBottomSheetDialogFragment {
+            return BaseBottomSheetDialogFragment().apply {
+                this.childFragment = childFragment
+            }
+        }
+
+        fun newInstance(childFragment: Fragment, animationRes: Int): BaseBottomSheetDialogFragment {
+            return BaseBottomSheetDialogFragment().apply {
+                this.childFragment = childFragment
+                this.animationRes = animationRes
+            }
+        }
+
+        fun newInstance(
+            childFragment: Fragment,
+            isHideBottomToggleView: Boolean? = false
+        ): BaseBottomSheetDialogFragment {
+            return BaseBottomSheetDialogFragment().apply {
+                isHideBottomToggleView?.let {
+                    arguments = Bundle().apply {
+                        putBoolean(KEY_HIDE_BOTTOM_TOGGLE_VIEW, isHideBottomToggleView)
+                    }
+                }
+                this.childFragment = childFragment
+            }
+        }
+    }
+}
+
+fun Fragment.dismissBottomSheet() {
+    (parentFragment as? BaseBottomSheetDialogFragment)?.dismiss()
+}
+
+fun Fragment.setCancelable(isCancelable: Boolean) {
+    (parentFragment as? BaseBottomSheetDialogFragment)?.isCancelable = isCancelable
 }
